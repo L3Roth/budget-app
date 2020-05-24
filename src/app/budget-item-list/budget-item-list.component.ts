02e8a49c712fd92696e7e840473a043efb919341
4l3xskillset <alexander.noack95@outlook.de>
2020-05-24 22:59:52 +0200

    material design installed, beginning item-modal

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,3 +1,5 @@
+import { EditItemModalComponent } from './../edit-item-modal/edit-item-modal.component';
+import { MatDialog } from '@angular/material';
 import { BudgetItem } from './../../shared/models/budget-item.module';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
@@ -11,13 +13,21 @@ export class BudgetItemListComponent implements OnInit {
   @Input() budgetItems: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   onDeleteButtonClicked(item: BudgetItem) {
     this.delete.emit(item);
+  }
+
+  onCardClicked(item: BudgetItem) {
+    //show edit module
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item
+    });
   }
 
 }
